e920028caf5777dd82d8fe040897d692d66d2abb
ikawaha <ikawaha@users.noreply.github.com>
2020-04-24 18:24:03 +0900

    Fix uid/gid

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+
+if [ -z "$USER_ID" ]; then
+usermod -u $USER_ID -o -m firefox
+fi
+if [ -z "$GROUP_ID" ]; then
+groupmod -g $GROUP_ID firefox
+fi
+
 ip=$(hostname --ip-address)
 
 socat tcp-listen:2828,bind="$ip",fork tcp:127.0.0.1:2828 &
